460eb5dae8173d8f6299f11e529ee18bdfef8ca6
ahdinosaur <ahdinosaur@users.noreply.github.com>
2021-09-25 13:01:54 +0000

    deploy: dbc151f0c4fed832e48024b33ea0aa191de6b9ba

---
+++ component---src-pages-404-js-70331ec28d39b88899bd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{229:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(228),o=n(152);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"404: NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist..."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-70331ec28d39b88899bd.js.map
